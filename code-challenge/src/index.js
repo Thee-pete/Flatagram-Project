9c74f48aa5ae556dd4058c278d454b2c8148a741
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-20 20:59:08 +0300

    all changes

--- code-challenge/src/index.js
+++ code-challenge/src/index.js
@@ -1,26 +1,28 @@
-// write your code here
-// var likeCount = document.getElementById("#like-count")
-// function count(){
-//     likeCount.innerText = parseInt(likeCount.innerText) + 1;
 
-// }
-// console.log(count);
-// const likes = document.querySelectorAll(".likes");
-// console.log(likes);
-// likes.forEach(function(like){
-//     like.addEventListener('click',function(){
-//         console.log(like.childNodes[0]);
-//         if(like.childNode[0].classList.contains('far')){
-//             console.log("Object")
-//         }  
-//     })
-// })
-fetch("http://localhost:3000/posts")
-.then((resp) => resp.json())
-// .then((currentLikeData) =>
-// setLikeCounter(currentLikeData, id, setLikeCounter)
-// );
-var likeButton = document.getElementById("like-button")
-likeButton.addEventListener("click", function(){
-    document.getElementById("like-count").innerHTML = "like 1";
+function updateLikes(){
+    
+    fetch ("http://localhost:3000/images/1")
+    .then(resp => resp.json())
+    .then(data => {
+       
+        document.getElementById('like-button').addEventListener('click', ()=> {
+    
+            data.likes +=1 
+
+            document.getElementById('like-count').textContent = data.likes
+        
+            fetch  ("http://localhost:3000/images/1", {
+            method: 'PATCH',
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({
+                "likes" : data.likes
+            })
+
+        })
+    })
 })
+
+}
+updateLikes();
