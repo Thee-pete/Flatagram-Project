f0a30cb66320dddfb80c5af4d3076e0f381a3198
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-20 19:08:10 +0300

    image rendering, hiding and changing

--- code-challenge/src/index.js
+++ code-challenge/src/index.js
@@ -1,6 +1,8 @@
 // write your code here
 document.addEventListener('DOMContentLoaded', function() {
-    displayImageAndDetails();
+    
+    doAll()
+  
 });
    
 let imgTitle = document.getElementById('card-title')
@@ -9,35 +11,73 @@ let imgLikesCount = document.getElementById('like-count')
 let imgCommentList = document.getElementById('comments-list')
 
 
+function doAll(){
+    displayImageAndDetails()
+    hideImage()
+    changeDog()
+    updateLikes()
+    
+}
+
+
 function displayImageAndDetails(){
     const imageDetails = fetch ("http://localhost:3000/images/1")
     .then(resp => resp.json())
     .then(data => renderImageAndDetails(data))
-   
-
     return imageDetails
 }
 
 function renderImageAndDetails(imageDetails){
-    
         imgTitle.textContent = imageDetails.title;
         imgSrc.src = imageDetails.image;
-        imgLikesCount.textContent =imageDetails.likes + ' likes';
+        imgLikesCount.textContent =imageDetails.likes;
         fetchComments(imageDetails.comments);
-
-                
+     
+        
 }
 function fetchComments(commentsArray){
-
     commentsArray.forEach(element => {  
     let comment = document.createElement('li')
     comment.appendChild(document.createTextNode(element.content))
-    imgCommentList.appendChild(comment)
-  
-          
+    imgCommentList.appendChild(comment)          
     })
-  
-    
-   
+       
 }
+function hideImage(){
+    document.getElementById('card-title').addEventListener('click', ()=> {
+        let cardImg = document.getElementById('card-image')
+        if (cardImg.style.display === "none") {
+            cardImg.style.display = "block";
+          } else {
+            cardImg.style.display = "none";
+          }
+    })
+}
+function changeDog(){
+    document.getElementById('card-image').addEventListener('click', () =>{
+        fetch ("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(data => {
+
+            fetch ("http://localhost:3000/images/1", {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type' : 'application/json'
+    
+                },
+                body: JSON.stringify({
+                    "image": data.message
+                })
+            })
+
+        })
+       
+    })
+}
+
+
+
+
+
+
 
