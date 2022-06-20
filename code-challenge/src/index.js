539d45d2ff3298ea6f59e4f56385ed2b0fd47c8f
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-20 16:02:48 +0300

    Done & working

--- code-challenge/src/index.js
+++ code-challenge/src/index.js
@@ -7,7 +7,7 @@ let imgTitle = document.getElementById('card-title')
 let imgSrc = document.getElementById('card-image')
 let imgLikesCount = document.getElementById('like-count')
 let imgCommentList = document.getElementById('comments-list')
-let comment = document.createElement('li')
+
 
 function displayImageAndDetails(){
     const imageDetails = fetch ("http://localhost:3000/images/1")
@@ -29,8 +29,7 @@ function renderImageAndDetails(imageDetails){
 }
 function fetchComments(commentsArray){
 
-    commentsArray.forEach(element => {
-  
+    commentsArray.forEach(element => {  
     let comment = document.createElement('li')
     comment.appendChild(document.createTextNode(element.content))
     imgCommentList.appendChild(comment)
