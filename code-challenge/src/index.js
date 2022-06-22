1006663fa0027e3b0d73cef0303446c66760087c
Peter Ndumia <ndegwandumia@gmail.com>
2022-06-22 12:42:56 +0300

    Done with Challenge

--- code-challenge/src/index.js
+++ code-challenge/src/index.js
@@ -1,172 +1,137 @@
-
-
-let imgTitle = document.getElementById('card-title')
-let imgSrc = document.getElementById('card-image')
-let imgLikesCount = document.getElementById('like-count')
-let imgCommentList = document.getElementById('comments-list')
+let imgTitle = document.getElementById("card-title");
+let imgSrc = document.getElementById("card-image");
+let imgLikesCount = document.getElementById("like-count");
+let imgCommentList = document.getElementById("comments-list");
 //Getting the image, title and the comments (GET METHOD)
 
-function displayImageAndDetails(){
-    const imageDetails = fetch ("http://localhost:3000/images/1")
-    .then(resp => resp.json())
-    .then(data => renderImageAndDetails(data))
-    return imageDetails
+function displayImageAndDetails() {
+  const imageDetails = fetch("http://localhost:3000/images/1")
+    .then((resp) => resp.json())
+    .then((data) => renderImageAndDetails(data));
+  return imageDetails;
 }
-displayImageAndDetails() 
+displayImageAndDetails();
 
-
-function renderImageAndDetails(imageDetails){
-        imgTitle.textContent = imageDetails.title;
-        imgSrc.src = imageDetails.image;
-        imgLikesCount.textContent =`${imageDetails.likes} likes`;
-        fetchComments(imageDetails.comments);
-     
-        
+function renderImageAndDetails(imageDetails) {
+  imgTitle.textContent = imageDetails.title;
+  imgSrc.src = imageDetails.image;
+  imgLikesCount.textContent = `${imageDetails.likes} likes`;
 }
-function fetchComments(commentsArray){
-    commentsArray.forEach(element => {  
-    let comment = document.createElement('li')
-    comment.appendChild(document.createTextNode(element.content))
-    imgCommentList.appendChild(comment)          
-    })
-       
-}
-
-
 
 // Hiding the image after clicking on the title
 
-function hideImage(){
-    document.getElementById('card-title').addEventListener('click', ()=> {
-        let cardImg = document.getElementById('card-image')
-        if (cardImg.style.display === "none") {
-            cardImg.style.display = "block";
-          } else {
-            cardImg.style.display = "none";
-          }
-
-
-
-    })
+function hideImage() {
+  document.getElementById("card-title").addEventListener("click", () => {
+    let cardImg = document.getElementById("card-image");
+    if (cardImg.style.display === "none") {
+      cardImg.style.display = "block";
+    } else {
+      cardImg.style.display = "none";
+    }
+  });
 }
-hideImage()
+hideImage();
 
 // Changing the image of the dog after clicking on the image
 
-function changeDog(){
-    document.getElementById('card-image').addEventListener('click', () =>{
-        fetch ("https://dog.ceo/api/breeds/image/random")
-        .then(resp => resp.json())
-        .then(data => {
-
-            fetch ("http://localhost:3000/images/1", {
-                method: 'PATCH',
-                headers: {
-                    'Content-Type' : 'application/json'
-    
-                },
-                body: JSON.stringify({
-                    "image": data.message
-                })
-            })
-
-        })
-       
-    })
+function changeDog() {
+  document.getElementById("card-image").addEventListener("click", () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((resp) => resp.json())
+      .then((data) => {
+        fetch("http://localhost:3000/images/1", {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            image: data.message,
+          }),
+        });
+      });
+  });
 }
-changeDog()
+changeDog();
 
-// like button 
+// like button
 const toggleHeart = () => {
-  const likeButton = document.querySelector("#like-button")
-    
-  likeButton.addEventListener("click", (event)  =>{
- if(likeButton.textContent === "♥"){
-    likeButton.textContent = "♡"
- } else {
-    likeButton.textContent = "♥";
-    likeCounter();
- }
- })
-}
+  const likeButton = document.querySelector("#like-button");
 
-let n=0
+  likeButton.addEventListener("click", (event) => {
+    if (likeButton.textContent === "♥") {
+      likeButton.textContent = "♡";
+    } else {
+      likeButton.textContent = "♥";
+      likeCounter();
+    }
+  });
+};
+
+let n = 0;
 const likeCounter = () => {
- const   likeCount = document.querySelector("#like-count");
- n++;
- likeCount.textContent = `${n} likes`
-}
+  const likeCount = document.querySelector("#like-count");
+  n++;
+  likeCount.textContent = `${n} likes`;
+};
 
 toggleHeart();
 
+//  comment
+const ul = document.querySelector("#comments-list");
+const commentSection = () => {
+  fetch("http://localhost:3000/comments")
+    .then((response) => response.json())
+    .then((data) => fetchComments(data));
+};
 
-//  comment 
-const ul = document.querySelector('#comments-list');
-const commentSection = ()=>{
-    fetch("http://localhost:3000/comments")
-    .then((response)=>response.json())
-    .then((data)=>fetchComments(data))
-}
-
-const fetchComments = (information)=>{
-    information.forEach((element)=>{
-        const li = document.createElement('li');
-        li.textContent = `${element.content}`;
-        ul.appendChild(li);
-        removeComments(li,element.id);
-    })
-}
+const fetchComments = (information) => {
+  information.forEach((element) => {
+    const li = document.createElement("li");
+    li.textContent = `${element.content}`;
+    ul.appendChild(li);
+    removeComments(li, element.id);
+  });
+};
 
 commentSection();
 
-const addComments = ()=>{
-    const form = document.querySelector('#comment-form');
-    const inputBar = document.querySelector('.comment-input');
-    form.addEventListener('submit',(event)=>{
-        event.preventDefault();
-        const li = document.createElement('li');
-        li.textContent = inputBar.value;
-        ul.appendChild(li);
-        postComments(inputBar.value);
-        removeComments(li);
-    })
-}
+const addComments = () => {
+  const form = document.querySelector("#comment-form");
+  const inputBar = document.querySelector(".comment-input");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const li = document.createElement("li");
+    li.textContent = inputBar.value;
+    ul.appendChild(li);
+    postComments(inputBar.value);
+    removeComments(li);
+  });
+};
 
-addComments()
+addComments();
 
-const postComments = (comment)=>{
-    fetch("http://localhost:3000/comments",{
-        method:'POST',
-        headers:{
-            'Content-Type':'application/json',
-        },
-        body:JSON.stringify({
-            "imageId": 1,
-            "content":`${comment}`
-        })
-    })
-}
+const postComments = (comment) => {
+  fetch("http://localhost:3000/comments", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      imageId: 1,
+      content: `${comment}`,
+    }),
+  });
+};
 
+const removeComments = (listItem, item) => {
+  listItem.addEventListener("click", (event) => {
+    listItem.remove();
+    deleteComments(item);
+  });
+};
 
-const removeComments = (listItem,item)=>{
-    listItem.addEventListener('click',(event)=>{
-        listItem.remove();
-        deleteComments(item);
-    })
-}
-
-const deleteComments = (itemToDelete)=>{
-    fetch(`http://localhost:3000/comments/${itemToDelete}`,{
-        method:'DELETE'
-    })
-}
-
-
-
-
-
-
-
-
-
-
+const deleteComments = (itemToDelete) => {
+  fetch(`http://localhost:3000/comments/${itemToDelete}`, {
+    method: "DELETE",
+  });
+};
